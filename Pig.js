5246ead28b4ff82a970acb29b8ebf221ddf6b7fc
IshanviSeth <ishanviseth10@gmail.com>
2020-09-21 18:50:43 +0530

    Trajectory

--- Pig.js
+++ Pig.js
@@ -6,7 +6,6 @@ class Pig extends BaseClass {
   }
 
  display(){
-   console.log(this.body.speed);
    if(this.body.speed < 3){
     super.display();
    }
@@ -20,7 +19,4 @@ class Pig extends BaseClass {
    }
    
  }
-
-
-
 };
